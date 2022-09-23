00e4e50f5a3cb1b4fdc7889a870d9f7e30e95e37
josh-leebrowne <josh.lv.lee@gmail.com>
2022-09-22 23:18:20 -0500

    address

---
+++ week6/addressbook/addressbook/contacts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+add_name = "contact"
+
+urlpatterns = [
+    
+    path('', views.show_all_contacts ),
+    path('<int:contactid>',  views.show_contact ),    
+
+]
